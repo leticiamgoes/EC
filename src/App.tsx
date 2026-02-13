65236f086df303a5bf17b42b43547e883479eda9
letciamgoes <monteirogoes01@gmail.com>
2026-02-13 13:52:27 -0300

    mudanca app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -20,9 +19,9 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/minicurso" element={<MiniCurso />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
-            <Route path="/minicurso" element={<MiniCurso />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
